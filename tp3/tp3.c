10dd7dc7ddb244042500a11c44b52316ea7aeb88
sebasgm1 <165147010+sebasgm1@users.noreply.github.com>
2024-10-11 13:13:45 -0300

    tp3

---
+++ tp3/tp3.c
@@ -0,0 +1,21 @@
+/* 
+ * Tipos Abstratos de Dados - TADs
+ * Arquivo do programa principal, que usa o TAD racional.
+ * Feito em 24/09/2024 para a disciplina CI1001 - Programação 1.
+*/
+
+/* coloque aqui seus includes (primeiro os <...>, depois os "...") */
+#include <stdio.h>
+#include <stdlib.h>
+#include "racional.h"
+
+/* coloque aqui as funções auxiliares que precisar neste arquivo */
+
+/* programa principal */
+int main ()
+{
+  /* a completar! */
+
+  return (0) ;
+}
+
