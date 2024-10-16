d5af70137936ac51bccd594a64210c6ace5fea53
sebasgm1 <165147010+sebasgm1@users.noreply.github.com>
2024-10-15 21:35:32 -0300

    foi?

--- tp3/tp3.c
+++ tp3/tp3.c
@@ -1,15 +1,37 @@
-/* 
- * Tipos Abstratos de Dados - TADs
- * Arquivo do programa principal, que usa o TAD racional.
- * Feito em 24/09/2024 para a disciplina CI1001 - Programação 1.
-*/
-
-/* coloque aqui seus includes (primeiro os <...>, depois os "...") */
 #include <stdio.h>
 #include <stdlib.h>
 #include "racional.h"
 
 /* coloque aqui as funções auxiliares que precisar neste arquivo */
+
+void imprime_vet (struct racional *v[], int n) {
+  printf ("VETOR =");
+  for (int i = 0; i<n; i++) {
+    imprime_r (&v[i]);
+  }
+  printf ("\n");  
+}
+
+void troca (struct racional *a, struct racional *b) {
+  struct racional temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+
+void bubblesort (struct racional v[], int n) {
+  int trocou = 1;
+  while (trocou) {
+    trocou = 0;
+    for (int i = 0; i<n-1; i++) {
+      if (compara_r (&v[i], &v[i+1]) == 1) {
+        troca (&v[i], &v[i+1]);
+        trocou = 1;
+      }
+    }
+  }
+}
+
 
 /* programa principal */
 int main ()
@@ -18,39 +40,76 @@ int main ()
   scanf ("%d", &n);
 
   // aloque dinamicamente um vetor com n ponteiros para números racionais
-  struct racional **vetor;
-  vetor[n] = malloc (n * sizeof (struct racional));
+  // preencha o vetor com n números racionais lidos da entrada
+  // (leia o numerador e o denominador de cada racional)
 
-  if (vetor == NULL) {
+  struct racional **v = malloc (n * sizeof (struct raciona*));
+
+  if (v == NULL) {
     printf ("nao conseguiu alocar o vetor\n");
     return 1;
   }
   for (int i = 0; i<n; i++) {
     long a, b;
-    vetor[i] = malloc (sizeof(struct racional));
-
     scanf ("%ld, %ld", &a, &b);
-    vetor[i] = cria_r (a, b);
+
+    v[i] = cria_r (a, b); /* ponteiro recebendo endereço de outro ponteiro */
   }
-  // preencha o vetor com n números racionais lidos da entrada
-  // (leia o numerador e o denominador de cada racional)
 
   // imprima "VETOR = " e os racionais apontados pelo vetor
+  imprime_vet (&v, n);
 
   // elimine do vetor os racionais inválidos
   // imprima "VETOR = " e o vetor resultante
+  for (int i=0; i<n; i++) {
+    if (!valido_r(v[i])) {
+      while (!valido_r(v[n-1])) { /* testa se a ùltima posição é valida até achar uma válida */
+        n--;
+      }
+      if (i != n) { /* esse if impede que o último caso se elimine, mesmo sendo válido */
+        v[i] = v[n-1];
+        n--;
+      }
+    }
+  }
 
   // ordene o vetor em ordem crescente
   // imprima "VETOR = " e os racionais apontados pelo vetor
+  bubblesort (v, n);
+  imprime_vet (&v, n);
+
 
   // calcule a soma dos racionais apontados pelo vetor
   // imprima "SOMA = " e a soma calculada acima
+  struct racional *r3 = cria_r (0, 1);
+  for (int i = 0; i<n; i++) {
+    if (!soma_r (&r3, &v[i], &r3))
+    printf ("nao foi somado\n");
+  }
+
+  printf ("SOMA = ");
+  imprime_r (r3);
+  printf ("\n");
+
 
   // libere os racionais apontados pelo vetor
   // imprima "VETOR = " e os racionais apontados pelo vetor
+    for (int i = 0; i<n; i++) {
+      destroi_r (&&v[i]);
+  }
+  imprime_vet (&v, n); /* se der erro vai ser AQUI */
+
 
   // libere o vetor de ponteiros
   // libere o espaço utilizado para fazer o cálculo da soma
+  for (int i = 0; i<n; i++) {
+    if (v[i] != NULL) {
+      destroi_r (&v[i]);
+      v[i] = NULL;
+    }
+  }
+  free(r3);
+  r3 = NULL;
+
   return (0) ;
 }
-
